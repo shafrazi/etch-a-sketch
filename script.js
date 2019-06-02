ae73bec64ac346d4f361c2769541b30a1c1501bf
shafrazi <shafrazi@live.com>
2019-06-03 00:34:39 +0530

    add script to create 16x16 squares

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+  const row = document.createElement("div");
+  row.className = "row";
+  container.append(row);
+  for (let j =0; j < 16; j++) {
+    const square = document.createElement("div");
+    square.className = "square";
+    row.append(square);
+  }
+}
