9a186d08fea94abf384d8b379c663807a0934986
shafrazi <shafrazi@live.com>
2019-06-03 02:29:06 +0530

    add specifying square nos

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("#btnSquares");
+const text = document.querySelector("#squareNo");
 
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  row.className = "row";
-  container.append(row);
-  for (let j =0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.className = "square";
-    row.append(square);
+
+button.addEventListener("click", function() {
+  container.innerHTML = "";
+  let squareWidth = `${(800 / parseInt(text.value)) - 1}px`;
+  for (let i = 0; i < parseInt(text.value); i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+    container.append(row);
+    for (let j =0; j < parseInt(text.value); j++) {
+      const square = document.createElement("div");
+      square.className = "square";
+      square.style.width = squareWidth;
+      square.style.height = squareWidth;
+      row.append(square);
+    }
   }
-}
+  text.value = "";
 
-const squares = document.querySelectorAll(".square");
+  const squares = document.querySelectorAll(".square");
 
-for (let i =0; i < squares.length; i++) {
-  let square = squares[i];
-  square.addEventListener("mouseover", function() {
-    square.classList.add("hover");
-  })
-}
+  for (let i =0; i < squares.length; i++) {
+    let square = squares[i];
+    square.addEventListener("mouseover", function() {
+      square.classList.add("hover");
+    })
+  }
+})
