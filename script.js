351ea0250064cfe0543ab449c9319fa1845860bf
shafrazi <shafrazi@live.com>
2019-06-03 23:08:51 +0530

    random color change on hover

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const container = document.querySelector(".container");
 const button = document.querySelector("#btnSquares");
 const text = document.querySelector("#squareNo");
 
-
 button.addEventListener("click", function() {
   container.innerHTML = "";
-  let squareWidth = `${(800 / parseInt(text.value)) - 1}px`;
-  for (let i = 0; i < parseInt(text.value); i++) {
+  let squareNo = parseInt(text.value);
+  let squareWidth = `${(800 / squareNo) - 1}px`;
+  for (let i = 0; i < squareNo; i++) {
     const row = document.createElement("div");
     row.className = "row";
     container.append(row);
-    for (let j =0; j < parseInt(text.value); j++) {
+    for (let j =0; j < squareNo; j++) {
       const square = document.createElement("div");
       square.className = "square";
       square.style.width = squareWidth;
@@ -25,7 +25,12 @@ button.addEventListener("click", function() {
   for (let i =0; i < squares.length; i++) {
     let square = squares[i];
     square.addEventListener("mouseover", function() {
-      square.classList.add("hover");
+      square.style.backgroundColor = `rgb(${randomColorCode()}, ${randomColorCode()}, ${randomColorCode()})`;
     })
   }
 })
+
+function randomColorCode() {
+  let randomCode = parseInt(Math.random() * 256) + 1;
+  return randomCode;
+};
